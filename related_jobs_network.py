5b13d220cf42fa339aad40bf439e2620cb3c387a
mlgzacfly <mlgzackfly@gmail.com>
2023-09-10 00:38:39 +0800

    feat: Encapsulate functionality into a class and allow user input

--- related_jobs_network.py
+++ related_jobs_network.py
@@ -3,61 +3,70 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import re
 import warnings
-warnings.filterwarnings("ignore", category=UserWarning)
 
-plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']
-plt.rcParams['axes.unicode_minus'] = False
 
-def get_related_jobs(job_id, depth, max_depth, graph, headers, parent_node=None):
-    if depth > max_depth:
-        return
-    if depth == 1:
-        parent_node = job_id
-    url = f"https://www.104.com.tw/job/ajax/similarJobs/{job_id}"
-    response = requests.get(url, headers=headers)
-    data = response.json()
-    similar_jobs = data['data']['list']
-    for job in similar_jobs:
-        related_job_id = job['link']['job']
-        related_job_name = job['name']
+class RelatedJobsGraph:
+    def __init__(self, start_job_id='83ix3', max_depth=3):
+        warnings.filterwarnings("ignore", category=UserWarning)  # 忽略警告
+        plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']
+        plt.rcParams['axes.unicode_minus'] = False
 
-        job_id_match = re.search(r"//www\.104\.com\.tw/job/(\w+)", related_job_id)
-        if job_id_match:
-            job_id = job_id_match.group(1)
+        self.start_job_id = start_job_id
+        self.max_depth = max_depth
+        self.headers = {
+            "Referer": f"https://www.104.com.tw/jobs/apply/analysis/{start_job_id}?jobsource=my104_apply"
+        }
+        self.G = nx.Graph()
+        self.G.add_node(start_job_id)
 
-            # 將新的節點添加到圖中，並使用工作名稱作為節點名稱
-            graph.add_node(related_job_name)
-            graph.add_edge(parent_node, related_job_name)  # 連接父節點和新節點
+    def get_related_jobs(self, job_id, depth, parent_node=None):
+        if depth > self.max_depth:
+            return
+        if depth == 1:
+            parent_node = job_id
+        url = f"https://www.104.com.tw/job/ajax/similarJobs/{job_id}"
+        response = requests.get(url, headers=self.headers)
+        data = response.json()
+        similar_jobs = data['data']['list']
+        for job in similar_jobs:
+            related_job_id = job['link']['job']
+            related_job_name = job['name']
 
-            # 遞迴深入下一層
-            get_related_jobs(job_id, depth + 1, max_depth, graph, headers, related_job_name)
-        else:
-            print("未找到工作ID")
+            job_id_match = re.search(r"//www\.104\.com\.tw/job/(\w+)", related_job_id)
+            if job_id_match:
+                job_id = job_id_match.group(1)
 
-# 起始工作 ID 和深度限制
-start_job_id = "83ix3"
-max_depth = 3  # 限制獲取相關工作的深度
+                # 將新的節點添加到圖中，並使用工作名稱作為節點名稱
+                self.G.add_node(related_job_name)
+                self.G.add_edge(parent_node, related_job_name)  # 連接父節點和新節點
 
-# 設置 Referer 標頭
-headers = {
-    "Referer": "https://www.104.com.tw/jobs/apply/analysis/83ix3?jobsource=my104_apply"
-}
+                # 遞迴深入下一層
+                self.get_related_jobs(job_id, depth + 1, related_job_name)
+            else:
+                print("未找到工作ID")
 
-# 創建一個空的關聯網路圖
-G = nx.Graph()
+    def draw_graph(self):
+        # 使用 Fruchterman-Reingold 布局算法
+        pos = nx.fruchterman_reingold_layout(self.G)
 
-# 將起始工作節點添加到圖中
-G.add_node(start_job_id)
+        # 設置圖的大小，根據節點數量自動調整
+        plt.figure(figsize=(50, 50))
 
-# 開始遞迴獲取相關工作
-get_related_jobs(start_job_id, 1, max_depth, G, headers)
+        # 繪製關聯網路圖
+        nx.draw(self.G, pos, with_labels=True, node_size=10000, node_color='skyblue', font_size=28)
+        plt.show()
 
-# 使用 Fruchterman-Reingold 布局算法
-pos = nx.fruchterman_reingold_layout(G)
 
-# 設置圖的大小，根據節點數量自動調整
-plt.figure(figsize=(50, 50))
+if __name__ == "__main__":
+    # 提示用戶輸入工作 ID 和深度
+    start_job_id = input("請輸入起始工作 ID（預設為 83ix3）：") or "83ix3"
+    max_depth = int(input("請輸入最大深度（預設為 3）：") or 3)
 
-# 繪製關聯網路圖
-nx.draw(G, pos, with_labels=True, node_size=10000, node_color='skyblue', font_size=28)
-plt.show()
+    # 創建 RelatedJobsGraph 類別的實例
+    graph = RelatedJobsGraph(start_job_id, max_depth)
+
+    # 開始遞迴獲取相關工作
+    graph.get_related_jobs(start_job_id, 1)
+
+    # 繪製關聯網路圖
+    graph.draw_graph()
